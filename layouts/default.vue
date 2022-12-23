ab4f2322ad8fa149a0b03da3d22eb8b1a3e8e8e8
Jam Wong <jamwong@ecosa.com.hk>
2022-12-23 17:47:53 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative min-h-screen bg-blue-50">
+  <div class="relative min-h-screen">
     <div class="sticky top-0 z-50 h-12 space-x-6 bg-white px-6 py-3">
       <!-- <NuxtLink to="/">home</NuxtLink>
       <NuxtLink to="/story">story</NuxtLink> -->
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <div class="relative flex justify-end">
-      <div class="z-20 mt-6 mr-6 mb-6 h-[150vh] w-[380px] rounded bg-sky-400 p-3">2</div>
+      <div
+        class="z-20 mt-16 mr-16 mb-16 flex h-[150vh] w-[380px] flex-col gap-3 rounded bg-white p-3 shadow-sm"
+      >
+        <div class="text-2xl text-blue-600">product name</div>
+        <div class="h-24 bg-red-50 p-3">box 1</div>
+        <div class="h-48 bg-orange-50 p-3">box 2</div>
+        <div class="h-24 bg-yellow-50 p-3">box 3</div>
+        <div class="h-12 bg-green-50 p-3">box 4</div>
+        <div class="h-24 bg-lime-50 p-3">box 5</div>
+        <div class="h-60 bg-purple-50 p-3">box 6</div>
+      </div>
 
       <div class="absolute inset-0 z-10 w-full bg-yellow-300">
-        <div class="sticky top-12 h-[900px] w-full bg-purple-300">
+        <div class="sticky top-12 h-[900px] min-h-[calc(100vh-3rem)] w-full bg-purple-300">
           <img
             class="h-full w-full object-cover object-center"
             :srcset="`https://ecosaau.imgix.net/846/pillow.jpg?w=3840 3840w,
@@ -18,6 +28,10 @@
         </div>
       </div>
     </div>
-    <div class="h-[20vh] bg-gray-100 p-3">below</div>
+    <div class="container mx-auto flex flex-col gap-8 py-8">
+      <div class="h-[20vh] bg-gray-100 p-3 text-2xl">section 1</div>
+      <div class="h-[20vh] bg-gray-200 p-3 text-2xl">section 2</div>
+      <div class="h-[20vh] bg-gray-100 p-3 text-2xl">section 3</div>
+    </div>
   </div>
 </template>
