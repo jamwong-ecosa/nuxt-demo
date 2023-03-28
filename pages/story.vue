3642a284a8c3055c8dee199daad15471f7bbec67
Jam Wong <jamwong@ecosa.com.hk>
2023-03-28 18:40:57 +0800

    update

--- pages/story.vue
+++ pages/story.vue
@@ -10,6 +10,7 @@ const storyblokApi = useStoryblokApi()
 
 const { data } = await useAsyncData('pillow', async () => {
   console.log('called')
+  $fetch('/api/hello')
   return await storyblokApi.get('cdn/stories/en-au/pillow')
 })
 </script>
